05686c9ccd4011f9b34be2c9d9e1d1f958b28573
Lưu Phạm <phamthithanhluu1990@gmail.com>
2025-02-13 19:43:45 +0700

    update

---
+++ ThietBiNhaBep_23TH0004/Controllers/Dashboard_23TH0004Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThietBiNhaBep_23TH0004.Controllers
+{
+    public class Dashboard_23TH0004Controller : Controller
+    {
+        // GET: Dashboard_23TH0004
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
